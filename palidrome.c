7bb69363608b48e5e98e5d3f22017a18148974e4
annam1239 <annamtajannam@gmail.com>
2025-05-15 15:01:46 +0530

    Create palidrome.c

---
+++ palidrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num,reverse=0,reminder,orginal;
+    printf("entrer the number int");
+    scanf("%d",&num);
+    while(num!=0){
+        reminder=num%10;
+        reverse=num*10+reminder;
+        num/=10;
+            if("num=orginal"){
+                printf("enter the number is palidrome");
+               } else {
+                    printf("enter the number is not palidrome");
+                }
+            }
+        }
